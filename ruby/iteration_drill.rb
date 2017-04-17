42e5f8e6a3db13ba19e03ed59fcdb9ef24b71cdc
Michael Watanabe <mikey.watanabe@gmail.com>
2017-04-17 12:01:38 -0400

    5.7

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -7,14 +7,24 @@ zombie_apocalypse_supplies = ["hatchet", "rations", "water jug", "binoculars",
 # printing each item in the array separated by an asterisk
 # ----
 
+zombie_apocalypse_supplies.each {|a| print a + " * "}
+
 # 2. In order to keep yourself organized, sort your zombie_apocalypse_supplies
 # in alphabetical order. Do not use any special built-in methods.
 # ----
+
+print zombie_apocalypse_supplies.sort_by {|word| word.downcase }
 
 # 3. Create a method to see if a particular item (string) is in the
 # zombie_apocalypse_supplies. Do not use any special built-in methods.
 # For instance: are boots in your list of supplies?
 # ----
+
+def search_array(array, search_item) {
+	print array.select {|a| a.match(search_item)}
+}
+
+search_array(zombie_apocalypse_supplies, "compass")
 
 # 4. You can't carry too many things, you've only got room in your pack for 5.
 # Remove items in your zombie_apocalypse_supplies in any way you'd like,
