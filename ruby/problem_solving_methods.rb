835d285c6772107c5841a120660a2baa222b071d
Michael Watanabe <mikey.watanabe@gmail.com>
2017-04-17 11:40:52 -0400

    5.6

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,16 @@
+
+
+integer_array = [1, 3, 5, 7, 10, 11, 15, 18, 21]
+
+numbers = integer_array 
+
+def integer_search(arg1=array, arg2=integer)
+
+arg1.each do 
+
+end
+
+integer_search(numbers,7)
+
+#comeback to this ---------
+
