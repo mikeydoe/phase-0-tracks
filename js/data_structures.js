821529bb7164f166ea8de9e61ff3c709c2cf8be3
Michael Watanabe <mikey.watanabe@gmail.com>
2017-04-24 10:47:09 -0400

    7.2

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["Blue", "Turquoise", "Black", "Scarlet"];
+
+var names = ["Jeffrey", "Bruce", "Jim", "Sweeney"];
+
+
+Map<String, String> horses = new HashMap<>();
+for (int i = 0; i < keys.length; i++) {
+   map.put(names[i], colors[i]);
+}
